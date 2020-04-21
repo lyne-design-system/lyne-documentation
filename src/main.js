a89b9022401170afb681254199b85c5d49f96f2d
feerglas <info@feerglas.com>
2020-04-21 12:49:15 +0200

    fix: ignore custom tags in vue again

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
-  // Vue.config.ignoredElements = [/lyne-\w*/];
+  Vue.config.ignoredElements = [/lyne-\w*/];
 
   head.script.push({
     src: 'https://unpkg.com/lyne-test/dist/lyne-components.js',
