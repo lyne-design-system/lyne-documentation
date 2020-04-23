7162855b8e00a58d8f4a06dcaaf7a7bd68830e1b
feerglas <info@feerglas.com>
2020-04-23 15:48:11 +0200

    fix: remove components link

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,9 +2,6 @@
   <Layout>
     <h1>Lyne Design System</h1>
 
-    <h2>Component Explorer</h2>
-    <g-link to="/lyne-components/">Components</g-link>
-
     <h2>Lyne Design System on GitHub</h2>
     <ul>
       <li>
