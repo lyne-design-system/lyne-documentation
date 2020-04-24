f7b65e701c1b802df54a0c8fc4f504e19349480d
feerglas <info@feerglas.com>
2020-04-24 09:56:27 +0200

    feat: add component for locale switcher

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <select
+    v-model="currentLocale"
+    @change="localeChanged"
+  >
+
+    <option
+      v-for="locale in availableLocales"
+      :key="locale"
+      :value="locale"
+    >{{ locale }}</option>
+
+  </select>
+
+</template>
+
+<script>
+
+export default {
+  data: (comp) => ({
+    availableLocales: comp._i18n.availableLocales,
+    currentLocale: comp._i18n.locale.toString()
+  }),
+  methods: {
+    localeChanged () {
+      this.$router.push({
+        path: this.$tp(this.$route.path, this.currentLocale, true)
+      });
+    }
+  },
+  name: 'LocaleSwitcher'
+};
+
+</script>
