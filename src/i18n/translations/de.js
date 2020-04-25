3bd63d8d618543a493423d806d4e39e81fdac9c7
feerglas <info@feerglas.com>
2020-04-25 10:20:23 +0200

    fix: remove paths

--- src/i18n/translations/de.js
+++ src/i18n/translations/de.js
@@ -1,9 +1,6 @@
-const paths = require('../paths/de');
-
 module.exports = {
   content: {
     translation1: 'Übersetzung 1',
     translation2: 'Übersetzung 2'
-  },
-  paths
+  }
 };
--- src/i18n/translations/en.js
+++ src/i18n/translations/en.js
@@ -1,9 +1,6 @@
-const paths = require('../paths/en');
-
 module.exports = {
   content: {
     translation1: 'Translation 1',
     translation2: 'Translation 2'
-  },
-  paths
+  }
 };
