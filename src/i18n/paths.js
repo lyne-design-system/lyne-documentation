2374c3bd748629666449915d33999ef17e383897
feerglas <info@feerglas.com>
2020-04-25 10:20:36 +0200

    feat: add paths file

---
+++ src/i18n/paths.js
@@ -0,0 +1,46 @@
+module.exports = {
+  de: {
+    components: {
+      path: 'komponenten',
+      template: 'Components',
+      title: 'Komponenten'
+    },
+    deployments: {
+      path: 'veroeffentlichungen',
+      template: 'Deployments',
+      title: 'Veröffentlichungen'
+    },
+    home: {
+      path: '',
+      template: 'Index',
+      title: 'Start'
+    },
+    playground: {
+      path: 'spielwiese',
+      template: 'Playground',
+      title: 'Spielwiese'
+    }
+  },
+  en: {
+    components: {
+      path: 'components',
+      template: 'Components',
+      title: 'Components'
+    },
+    deployments: {
+      path: 'deployments',
+      template: 'Deployments',
+      title: 'Deployments'
+    },
+    home: {
+      path: '',
+      template: 'Index',
+      title: 'Home'
+    },
+    playground: {
+      path: 'playground',
+      template: 'Playground',
+      title: 'Playground'
+    }
+  }
+};
