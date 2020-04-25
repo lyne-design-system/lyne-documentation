58b3ba251d1d1023c3a65944eb4f1a6306604b90
feerglas <info@feerglas.com>
2020-04-25 10:21:13 +0200

    feat: add event emitter

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -16,17 +16,43 @@
 </template>
 
 <script>
+const paths = require('../i18n/paths');
 
 export default {
   data: (comp) => ({
     availableLocales: comp._i18n.availableLocales,
-    currentLocale: comp._i18n.locale.toString()
+    currentLocale: comp._i18n.locale.toString(),
+    pathObject: paths[comp._i18n.locale.toString()]
   }),
   methods: {
     localeChanged () {
-      this.$router.push({
-        path: this.$tp(this.$route.path, this.currentLocale, true)
-      });
+      const currentPath = this.$route.path
+        .substring(this.currentLocale.length + 2, this.$route.path.length);
+      const cleanCurrentPath = currentPath.replace('/', '');
+
+      // find path object key which has clean currentPath as path
+      let keyFound = false;
+
+      Object.keys(this.pathObject)
+        .forEach((pathObjectKey) => {
+          const pathObject = this.pathObject[pathObjectKey];
+
+          if (pathObject.path === cleanCurrentPath) {
+            keyFound = pathObjectKey;
+          }
+        });
+
+      if (keyFound) {
+        const pathObjects = paths[this.currentLocale];
+        const desiredPath = `/${this.currentLocale}/${pathObjects[keyFound].path}`;
+
+        this.pathObject = pathObjects;
+        this.$router.push({
+          path: desiredPath
+        });
+
+        this.$emit('clicked', this.currentLocale);
+      }
     }
   },
   name: 'LocaleSwitcher'
