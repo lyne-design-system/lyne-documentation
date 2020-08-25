f51b7d43c39ee3ec73898e728cab46d29cc10416
feerglas <info@feerglas.com>
2020-08-25 16:34:51 +0200

    chore: parse design tokens

--- src/templates/Tokens.vue
+++ src/templates/Tokens.vue
@@ -8,6 +8,43 @@
 </page-query>
 
 <script>
+const designTokens = require('lyne-design-tokens/dist/js/tokens.umd.js');
+
+const generateTokens = (json, _finalJson) => {
+
+  const keys = Object.keys(json);
+  const finalJson = _finalJson || {};
+
+  while (keys.length > 0) {
+    const key = keys.pop();
+    const value = json[key];
+    const hasPathKey = Object.keys(value)
+      .includes('path');
+
+    if (hasPathKey) {
+      const newKey = value.path.join('-');
+      const newValue = value.value;
+
+      finalJson[newKey] = newValue;
+    } else {
+      generateTokens(value, finalJson);
+    }
+  }
+
+  return finalJson;
+
+};
+
+export default {
+  data() {
+    return {
+      tokens: {
+        color: generateTokens(designTokens.color),
+        fontSize: generateTokens(designTokens.size.font)
+      }
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
