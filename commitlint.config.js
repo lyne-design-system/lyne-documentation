419d1b24779aac9b8795e06c248b5004509c0df1
feerglas <info@feerglas.com>
2020-09-10 13:59:04 +0200

    chore: fix linting issue

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-conventional'
+  ],
   parserPreset: 'conventional-changelog-conventionalcommits'
 };
