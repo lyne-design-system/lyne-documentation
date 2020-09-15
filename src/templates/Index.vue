a2f0fdd1dcbe3297bfdc7d1ca14467fe96bf480c
feerglas <info@feerglas.com>
2020-09-15 09:58:50 +0200

    feat: use bulma

--- src/templates/Index.vue
+++ src/templates/Index.vue
@@ -1,36 +1,41 @@
 <template>
   <Layout>
-    <h1>Lyne Design System</h1>
+    <section class="section">
+      <div class="container">
+        <h1 class="title is-1">Lyne Design System</h1>
+        <h2 class="title is-2">Lyne Design System on GitHub</h2>
+        <div class="content">
+          <ul>
+            <li>
+              <a
+              href="https://github.com/lyne-design-system/lyne-components"
+              target="_blank"
+              rel="noopener"
+              >lyne-components</a>
+              : Base Components for Lyne Design System
+            </li>
 
-    <h2>Lyne Design System on GitHub</h2>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/lyne-design-system/lyne-components"
-          target="_blank"
-          rel="noopener"
-        >lyne-components</a>
-        : Base Components for Lyne Design System
-      </li>
+            <li>
+              <a
+              href="https://github.com/lyne-design-system/lyne-design-tokens"
+              target="_blank"
+              rel="noopener"
+              >lyne-design-tokens</a>
+              : Managing and publishing design tokens for the lyne design system
+            </li>
 
-      <li>
-        <a
-          href="https://github.com/lyne-design-system/lyne-design-tokens"
-          target="_blank"
-          rel="noopener"
-        >lyne-design-tokens</a>
-        : Managing and publishing design tokens for the lyne design system
-      </li>
-
-      <li>
-        <a
-          href="https://github.com/lyne-design-system/lyne-getting-started"
-          target="_blank"
-          rel="noopener"
-        >lyne-getting-started</a>
-        : Sample integrations for Lyne Design System
-      </li>
-    </ul>
+            <li>
+              <a
+              href="https://github.com/lyne-design-system/lyne-getting-started"
+              target="_blank"
+              rel="noopener"
+              >lyne-getting-started</a>
+              : Sample integrations for Lyne Design System
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
 
   </Layout>
 </template>
@@ -44,7 +49,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  color: $color;
-}
 </style>
