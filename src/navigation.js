c867fb9b1830c1e19d4c539f8f77ecf1b49dda82
feerglas <info@feerglas.com>
2020-09-20 10:04:01 +0200

    chore: add navigation items in new file

---
+++ src/navigation.js
@@ -0,0 +1,37 @@
+/* eslint-disable sort-keys */
+module.exports = {
+  home: {
+    path: '',
+    template: 'Index',
+    title: 'Home',
+    hideFromNav: true
+  },
+  components: {
+    path: 'components',
+    template: 'Components',
+    title: 'Components'
+  },
+  designTokens: {
+    path: 'tokens',
+    template: 'Tokens',
+    title: 'Design Tokens'
+  },
+  icons: {
+    path: 'icons',
+    template: 'Icons',
+    title: 'Icons'
+  },
+  deployments: {
+    path: 'deployments',
+    template: 'Deployments',
+    title: 'Deployments',
+    hideFromNav: true
+  },
+  playground: {
+    path: 'playground',
+    template: 'Playground',
+    title: 'Playground',
+    hideFromNav: true
+  }
+};
+/* eslint-enable sort-keys */
