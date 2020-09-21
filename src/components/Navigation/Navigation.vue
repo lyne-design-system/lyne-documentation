3b1dd37190cfe657705e05f57a21778b8f2e574f
feerglas <info@feerglas.com>
2020-09-21 13:29:09 +0200

    feat: construct parent paths of child nav items

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -7,6 +7,7 @@
       v-for="(item, index) in $data.navData"
       :key="index"
       :item="item"
+      :paths="$data.paths"
     ></NavigationItem>
   </ul>
 </template>
@@ -16,13 +17,44 @@ import eventBus from '../../helpers/eventBus';
 import navData from '../../navigation';
 import NavigationItem from './NavigationItem.vue';
 
+const getPathOfNavItem = (obj, search) => {
+  for (const item of Object.entries(obj)) {
+    const value = item[1];
+
+    if (value === search) {
+      // return [key];
+      return [value];
+    }
+
+    if (value && typeof value === 'object') {
+      const path = getPathOfNavItem(value, search);
+
+      if (path) {
+        if (value.path) {
+          return [
+            value.path,
+            ...path
+          ];
+        }
+
+        return path;
+      }
+    }
+  }
+
+  return false;
+};
+
 export default {
   components: {
     NavigationItem
   },
-  data: () => {
+  data() {
+    const parentPathsOfCurrentRoute = getPathOfNavItem(navData, this.$route.path);
+
     const data = {
       navData,
+      paths: parentPathsOfCurrentRoute,
       showMenu: false
     };
 
