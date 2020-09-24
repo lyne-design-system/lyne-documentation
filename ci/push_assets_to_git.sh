77693eefefeef7da46cb4db8c9a9b42515500bc8
feerglas <info@feerglas.com>
2020-09-24 11:33:20 +0200

    chore: remove flag

--- ci/push_assets_to_git.sh
+++ ci/push_assets_to_git.sh
@@ -5,10 +5,6 @@
 # this script is being made executable with
 # chmod ugo+x push_assets_to_git.sh
 
-# set flags for execution of this file:
-# -e: make sure command returns non-zero exit code as soon as somehting fails
-set -e
-
 # version file name written by .releaserc
 VERSION_FILE=dist/version.txt
 
