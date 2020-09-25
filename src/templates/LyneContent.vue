4884b6666d2be1f3ea2a2192ea5b7a74c6a792fd
feerglas <info@feerglas.com>
2020-09-25 16:46:46 +0200

    feat: add basic template for rendering md content

---
+++ src/templates/LyneContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout >
+    <section class="section">
+      <div  class="container">
+        <div class="content" v-html="$page.contentItem.content"></div>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<page-query>
+query LyneContent ($path: String){
+  contentItem: lyneContent (path: $path) {
+    content
+  }
+}
+</page-query>
+
+<style lang="scss">
+
+  .icon.icon-link {
+    display: none !important;
+  }
+
+</style>
