b26a26d49b113f21213aacb08edc166559c49b65
feerglas <info@feerglas.com>
2020-09-26 19:08:18 +0200

    chore: remove flex search plugin config

--- gridsome.config.js
+++ gridsome.config.js
@@ -48,21 +48,6 @@ module.exports = {
         typeName: globalConfig.graphqlComponentsReadmeTypeName
       },
       use: '@gridsome/source-filesystem'
-    },
-
-    // flexsearch plugin
-    {
-      options: {
-        collections: [
-          {
-            fields: ['title'],
-            indexName: 'Component',
-            typeName: 'Component'
-          }
-        ],
-        searchFields: ['title']
-      },
-      use: 'gridsome-plugin-flexsearch'
     }
   ],
   siteDescription: 'Documentation for Lyne Design System',
