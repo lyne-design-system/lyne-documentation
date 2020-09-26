d4eb10ac0bfb94b06ca23161a36448537f4119b8
feerglas <info@feerglas.com>
2020-09-26 19:04:39 +0200

    feat: add sample search page

---
+++ src/pages/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <input
+      id="search"
+      v-model="searchTerm"
+      class="input"
+      type="text"
+      placeholder="Search">
+    {{ searchResults }}
+  </Layout>
+</template>
+
+<script>
+export default {
+  computed: {
+    searchResults () {
+      const {
+        searchTerm
+      } = this;
+
+      if (searchTerm.length < 3) {
+        return [];
+      }
+
+      return this.$search.search({
+        limit: 5,
+        query: searchTerm
+      });
+    }
+  },
+  data: () => ({
+    searchTerm: ''
+  })
+};
+
+</script>
