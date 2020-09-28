6f349980e9103af561b8f3f8172a76066e70eff9
feerglas <info@feerglas.com>
2020-09-28 20:13:06 +0200

    feat: add sample page to showcase search component

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,36 +1,17 @@
 <template>
   <Layout>
-    <input
-      id="search"
-      v-model="searchTerm"
-      class="input"
-      type="text"
-      placeholder="Search">
-    {{ searchResults }}
+
+    <Search />
+
   </Layout>
 </template>
 
 <script>
+import Search from '../components/Search.vue';
+
 export default {
-  computed: {
-    searchResults () {
-      const {
-        searchTerm
-      } = this;
-
-      if (searchTerm.length < 3) {
-        return [];
-      }
-
-      return this.$search.search({
-        limit: 5,
-        query: searchTerm
-      });
-    }
-  },
-  data: () => ({
-    searchTerm: ''
-  })
+  components: {
+    Search
+  }
 };
-
 </script>
