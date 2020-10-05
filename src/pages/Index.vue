d03a7c50e5f4241a57b84e7da5283cb7fc1ffab5
Michael Zumstein <zumi@yokai.ch>
2020-10-05 15:32:40 +0200

    fix: restructure navigation

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,19 @@
         <h1 class="title is-1">Lyne Design System</h1>
 
         <div class="content">
-          <p>Some content will follow soon</p>
+          <p>Lyne Design System is <span class="tag is-info">🧪 experimental</span> at the moment — with enthusiasm. Do not use the project until it has left infancy.</p>
+          <p><a href="/about" class="button is-primary">Learn more about Lyne</a></p>
+
+
+          <ul>
+            <li><a href="/designing">Start designing</a></li>
+            <li><a href="/coding">Start coding</a></li>
+            <li><a href="/writing">Start writing</a></li>
+            <li><a href="/contributing">Start contributing</a></li>
+          </ul>
+
+          <h4>Notice</h4>
+          <p>The state of the Lyne documentation platform is <em>work in progress (WIP)</em> and is inspired by <a href="https://carbondesignsystem.com">Carbon</a>, <a href="https://duetds.com">Duet</a>, <a href="https://boltdesignsystem.com">Bolt</a>, <a href="https://spectrum.adobe.com">Spectrum</a>. The documentation platform uses some of their content until Lyne will be mature enough/we are enough contributors from different disciplines to provide a more holistic perspective on the topic and the necessary resources get provided to document them accordingly.</p>
         </div>
       </div>
     </section>
