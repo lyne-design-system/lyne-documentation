25e6b4be14117df09c9e44ae5f2d01908cc4fd34
Michael Zumstein <zumi@yokai.ch>
2020-10-05 15:51:06 +0200

    fix: add eslint max-len to paragraph

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,16 +7,14 @@
         <div class="content">
           <p>Lyne Design System is <span class="tag is-info">🧪 experimental</span> at the moment — with enthusiasm. Do not use the project until it has left infancy.</p>
           <p><a href="/about" class="button is-primary">Learn more about Lyne</a></p>
-
-
           <ul>
             <li><a href="/designing">Start designing</a></li>
             <li><a href="/coding">Start coding</a></li>
             <li><a href="/writing">Start writing</a></li>
             <li><a href="/contributing">Start contributing</a></li>
           </ul>
-
           <h4>Notice</h4>
+          <!-- eslint-disable-next-line max-len -->
           <p>The state of the Lyne documentation platform is <em>work in progress (WIP)</em> and is inspired by <a href="https://carbondesignsystem.com">Carbon</a>, <a href="https://duetds.com">Duet</a>, <a href="https://boltdesignsystem.com">Bolt</a>, <a href="https://spectrum.adobe.com">Spectrum</a>. The documentation platform uses some of their content until Lyne will be mature enough/we are enough contributors from different disciplines to provide a more holistic perspective on the topic and the necessary resources get provided to document them accordingly.</p>
         </div>
       </div>
