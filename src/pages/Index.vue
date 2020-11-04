74c5dc74bcfcd68fa7f178772b689cacf6c4f138
Michael Zumstein <zumi@yokai.ch>
2020-11-04 09:11:41 +0100

    docs: move disclaimer to buttom

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,6 @@
         <h1 class="title is-1">Lyne Design System</h1>
 
         <div class="content">
-          <p>Lyne Design System is <span class="tag is-info">🧪 experimental</span> at the moment — with enthusiasm. Do not use the project until it has left infancy.</p>
           <p><g-link to="/about/" class="button is-primary">Learn more about Lyne</g-link></p>
           <ul>
             <li><g-link to="/designing/">Start designing</g-link></li>
@@ -15,6 +14,7 @@
           <h4>Notice</h4>
           <!-- eslint-disable-next-line max-len -->
           <p>The state of the Lyne documentation platform is <em>work in progress (WIP)</em> and is inspired by <a href="https://carbondesignsystem.com">Carbon</a>, <a href="https://duetds.com">Duet</a>, <a href="https://boltdesignsystem.com">Bolt</a>, <a href="https://spectrum.adobe.com">Spectrum</a>. The documentation platform uses some of their content until Lyne will be mature enough/we are enough contributors from different disciplines to provide a more holistic perspective on the topic and the necessary resources get provided to document them accordingly.</p>
+          <p>Lyne Design System is <span class="tag is-info">🧪 experimental</span> at the moment — with enthusiasm. Do not use the project until it has left infancy.</p>
         </div>
       </div>
     </section>
