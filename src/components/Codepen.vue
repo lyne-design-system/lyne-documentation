eb34782200dab3c441910e837efe7bc12314800e
feerglas <info@feerglas.com>
2021-01-29 15:08:42 +0100

    feat: add component for codepen button

---
+++ src/components/Codepen.vue
@@ -0,0 +1,45 @@
+<template>
+  <form
+    action="https://codepen.io/pen/define"
+    method="POST"
+    target="_blank"
+  >
+    <input
+      type="hidden"
+      name="data"
+      :value="JSON.stringify(contents)"
+    >
+
+    <span class="button">
+      <input
+        class="input-button"
+        type="submit"
+        value="Edit on Codepen.io"
+      >
+      <span class="icon is-small">
+        <i class="mdi mdi-launch"></i>
+      </span>
+    </span>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Codepen',
+  props: ['contents']
+};
+</script>
+
+<style lang="scss" scoped>
+.input-button {
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  background: none;
+  border: none;
+  outline: none;
+  padding: 0;
+  color: currentColor;
+  font: inherit;
+  cursor: pointer;
+
+}
+</style>
