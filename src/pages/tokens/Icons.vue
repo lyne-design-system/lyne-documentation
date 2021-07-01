edcaf4a37c697ab93b830f076b46d040cec82fcc
Yves Tscherry <info@feerglas.com>
2021-07-01 09:40:24 +0200

    fix: add radios to switch icon variants

--- src/pages/tokens/Icons.vue
+++ src/pages/tokens/Icons.vue
@@ -4,6 +4,27 @@
       <div class="container">
         <h1 class="title is-1">Icons</h1>
 
+        <div class="block">
+            <b-radio v-model="size"
+                @change.native="handleRadioChange('small')"
+                name="size"
+                native-value="small">
+                Small
+            </b-radio>
+            <b-radio v-model="size"
+                @change.native="handleRadioChange('medium')"
+                name="size"
+                native-value="medium">
+                Medium
+            </b-radio>
+            <b-radio v-model="size"
+                @change.native="handleRadioChange('large')"
+                name="size"
+                native-value="large">
+                Large
+            </b-radio>
+        </div>
+
         <div class="content">
           <table class="table is-fullwidth">
             <thead>
@@ -23,7 +44,7 @@
                     class='iconToken'
                   ></span>
                 </td>
-                <td class="textCell">{{icon.name}}</td>
+                <td class="textCell">{{icon.fullName}}</td>
               </tr>
             </tbody>
           </table>
@@ -37,11 +58,29 @@
 const lyneIcons = require('lyne-icons/dist/icons.json').icons;
 const sortHelper = require('../../helpers/sort');
 
+const defaultVariant = 'small';
+const filterIconsBySizeVariant = (size) => lyneIcons.filter((icon) => icon.variant === size);
+
+const getIcons = (size) => {
+  const iconsForVariant = filterIconsBySizeVariant(size);
+  const sortedIcons = sortHelper(iconsForVariant, 'name');
+
+  return sortedIcons;
+};
+
+const icons = getIcons(defaultVariant);
+
 export default {
   data() {
     return {
-      icons: sortHelper(lyneIcons, 'name')
+      icons,
+      size: defaultVariant
     };
+  },
+  methods: {
+    handleRadioChange(size) {
+      this.$data.icons = getIcons(size);
+    }
   }
 };
 </script>
