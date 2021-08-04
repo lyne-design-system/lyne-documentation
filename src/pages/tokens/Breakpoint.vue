91dee0849b1e2748f1b034e27bbc261751fe0f22
Yves Tscherry <info@feerglas.com>
2021-08-04 19:50:30 +0200

    feat: add breakpoint tokens

--- src/pages/tokens/Breakpoint.vue
+++ src/pages/tokens/Breakpoint.vue
@@ -2,8 +2,47 @@
   <Layout>
     <section class="section">
       <div class="container">
-        <h1 class="title is-1">Breakpoint</h1>
+        <div class="content">
+          <h1 class="title is-1">Breakpoint</h1>
+
+          <TokensTable
+            :tokens="$data.tokens"
+            :hideExampleColumn="true"
+          />
+
+        </div>
       </div>
     </section>
+
   </Layout>
 </template>
+
+<script>
+import * as tokens from 'lyne-design-tokens/dist/js/tokens.umd';
+import findTokens from '../../helpers/designToken';
+import TokensTable from '../../components/TokensTable.vue';
+
+export default {
+  components: {
+    TokensTable
+  },
+  data() {
+    const breakpointTokens = findTokens(tokens.breakpoint);
+
+    return {
+      tokens: breakpointTokens.reverse()
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.token {
+  display: block;
+  width: 4rem;
+  height: 4rem;
+  border: 1px solid black
+}
+
+</style>
