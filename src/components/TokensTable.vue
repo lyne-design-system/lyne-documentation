be4367ca8f4db976e963988413f91fbb046c2b1f
Yves Tscherry <info@feerglas.com>
2021-08-04 19:35:17 +0200

    chore: add component to render token tables

---
+++ src/components/TokensTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <table class="table is-fullwidth">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Value</th>
+        <th>Example</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(token, index) in tokens"
+        :key="index"
+      >
+        <td class="textCell">{{ token.fullName }}</td>
+        <td class="textCell">{{ token.value }}</td>
+        <td>
+          <slot v-bind:token="token"></slot>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TokensTable',
+  props: ['tokens']
+};
+</script>
+
+<style lang="scss" scoped>
+table td.textCell {
+  line-height: 100%;
+  vertical-align: middle;
+}
+
+</style>
