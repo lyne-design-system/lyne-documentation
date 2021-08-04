52069d4b7bdfcd38ddc3536ba515fc86bad27842
Yves Tscherry <info@feerglas.com>
2021-08-04 16:40:11 +0200

    feat:  add empty page for typo

---
+++ src/pages/tokens/Typo.vue
@@ -0,0 +1,9 @@
+<template>
+  <Layout>
+    <section class="section">
+      <div class="container">
+        <h1 class="title is-1">Typo</h1>
+      </div>
+    </section>
+  </Layout>
+</template>
