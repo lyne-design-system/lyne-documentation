d8c3d4748e74a20c0f8955cc0df8a3290be75299
Yves Tscherry <info@feerglas.com>
2021-08-04 19:41:49 +0200

    fix: use tokens table and add appropriate stylings

--- src/pages/tokens/Spacing.vue
+++ src/pages/tokens/Spacing.vue
@@ -2,41 +2,28 @@
   <Layout>
     <section class="section">
       <div class="container">
-        <div class="content">
-          <h1 class="title is-1">Spacing</h1>
+        <h1 class="title is-1">Spacing</h1>
 
-          <div
-            v-for="(category, index) in $data.tokens"
-            :key="index"
-          >
-            <h2>{{category.title}}</h2>
-
-            <table class="table is-fullwidth">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Value</th>
-                <th>Example</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(token) in category.values"
-                :key="token.key"
-              >
-                <td class="textCell">{{token.fullName}}</td>
-                <td class="textCell">{{token.value}}</td>
-                <td>
-                  <span
-                    class="list-item-sample"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          </div>
+        <div
+          class="content"
+          v-for="(category, index) in $data.tokens"
+          :key="index"
+        >
+          <h2>{{category.title}}</h2>
+          <TokensTable :tokens="category.values">
+            <template v-slot="slotProps">
+              <span
+                class="token"
+                :style="{
+                  height: slotProps.token.value,
+                  width: slotProps.token.value
+                }"
+              />
+            </template>
+          </TokensTable>
         </div>
       </div>
+
     </section>
   </Layout>
 </template>
@@ -44,8 +31,12 @@
 <script>
 import * as tokens from 'lyne-design-tokens/dist/js/tokens.umd';
 import findTokens from '../../helpers/designToken';
+import TokensTable from '../../components/TokensTable.vue';
 
 export default {
+  components: {
+    TokensTable
+  },
   data() {
     const spacingTokens = findTokens(tokens.spacing);
     const tokensFixed = spacingTokens.filter((token) => token.attributes.type === 'fixed');
@@ -65,5 +56,17 @@ export default {
       tokens: data
     };
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+
+.token {
+  display: block;
+  width: 4rem;
+  height: 4rem;
+  background-color: black;
+  box-sizing: border-box;
+}
+
+</style>
