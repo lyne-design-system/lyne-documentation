e25781b9aa82a0a2cad26c6626598e8909a2e390
Yves Tscherry <info@feerglas.com>
2021-08-08 10:01:22 +0200

    feat: add  method to add navlevels to navitems

--- src/helpers/navigation.js
+++ src/helpers/navigation.js
@@ -30,4 +30,58 @@ const getPathOfNavItem = (obj, search, usePathTitles) => {
   return false;
 };
 
-export default getPathOfNavItem;
+const checkNavItemLevel = (navItem) => {
+  let level = 0;
+
+  if (navItem.path) {
+    const split = navItem.path.split('/');
+
+    split.forEach((part) => {
+      const partLength = part.trim().length;
+
+      if (partLength > 0) {
+        level++;
+      }
+    });
+  }
+
+  return level;
+};
+
+const addLevelsToNavItems = (_nav) => {
+  const nav = _nav;
+
+  if (Array.isArray(nav)) {
+    nav.forEach((_navItem) => {
+      const navItem = _navItem;
+      const level = checkNavItemLevel(navItem);
+
+      if (level > 0) {
+        navItem.level = level;
+      }
+
+      addLevelsToNavItems(navItem);
+    });
+  } else if (typeof nav === 'object') {
+    const keys = Object.keys(nav);
+
+    keys.forEach((key) => {
+      const navItem = nav[key];
+
+      const level = checkNavItemLevel(navItem);
+
+      if (level > 0) {
+        navItem.level = level;
+      }
+
+      addLevelsToNavItems(navItem);
+    });
+  }
+
+  return nav;
+};
+
+export {
+  addLevelsToNavItems,
+  getPathOfNavItem
+};
