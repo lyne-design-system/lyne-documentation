d892e0b0fa0b8a581d89811c6e40f7baf29c47b2
Yves Tscherry <info@feerglas.com>
2021-08-14 17:35:07 +0200

    feat: add new size tokens

--- src/pages/tokens/Size.vue
+++ src/pages/tokens/Size.vue
@@ -2,24 +2,45 @@
   <Layout>
     <section class="section">
       <div class="container">
-        <h1 class="title is-1">Icon Size</h1>
+        <h1 class="title is-1">Size</h1>
 
-        <TokensTable :tokens="tokens">
-          <template v-slot="slotProps">
-            <span
-              class="icon-size-token"
-              v-html='getSampleIconForToken(slotProps.token)'
-              :style="{height: slotProps.token.value}"
-            />
-          </template>
-        </TokensTable>
+        <div
+          v-for="(key, index) in Object.keys($data.tokens)"
+          :key="index"
+        >
+          <h2 class="title is-2">{{key}}</h2>
+
+          <TokensTable :tokens="$data.tokens[key]">
+            <template v-slot="slotProps">
+              <span
+                v-if="key === 'icon'"
+                class="icon-size-token"
+                v-html='getSampleIconForToken(slotProps.token)'
+                :style="{height: slotProps.token.value}"
+              />
+
+              <span
+                v-if="key !== 'icon'"
+                class="default-size-token"
+                :style="{
+                  height: slotProps.token.value,
+                  width: slotProps.token.value
+                }"
+              />
+            </template>
+          </TokensTable>
+        </div>
+
       </div>
     </section>
   </Layout>
 </template>
 
 <script>
-import { designTokensByCategory } from '../../helpers/designToken';
+import {
+  designTokensByCategory,
+  groupedTokens
+} from '../../helpers/designToken';
 import TokensTable from '../../components/TokensTable.vue';
 
 const {
@@ -27,7 +48,7 @@ const {
 } = require('lyne-design-tokens/dist/js/tokens-raw.json');
 const lyneIcons = require('lyne-icons/dist/icons.json').icons;
 
-const sizeTokens = designTokensByCategory(tokens, 'iconSize');
+const sizeTokens = designTokensByCategory(tokens, 'size');
 
 sizeTokens.map((_token) => {
   const token = _token;
@@ -37,35 +58,37 @@ sizeTokens.map((_token) => {
   return token;
 });
 
+const tokensByGroups = groupedTokens(sizeTokens, 'group');
+
 export default {
   components: {
     TokensTable
   },
   data() {
     return {
-      iconUiSmall: lyneIcons['arrow-change-horizontal-small'],
-      tokens: sizeTokens.reverse()
+      tokens: tokensByGroups
     };
   },
   methods: {
     getSampleIconForToken(token) {
-      if (token.attributes.type === 'ui') {
-        return lyneIcons[`armchair-profile-user-${token.attributes.item}`];
+      console.log(token.attributes);
+      if (token.attributes.item === 'ui') {
+        return lyneIcons[`armchair-profile-user-${token.attributes.subitem}`];
       }
 
-      if (token.attributes.type === 'timetable') {
-        if (token.attributes.item === 'attribute') {
+      if (token.attributes.item === 'timetable') {
+        if (token.attributes.subitem === 'attribute') {
           return lyneIcons['sa-sk'];
         }
 
-        if (token.attributes.item === 'him-cus') {
+        if (token.attributes.subitem === 'him-cus') {
           return lyneIcons['cancellation'];
         }
 
         return lyneIcons['ic-11'];
       }
 
-      if (token.attributes.type === 'pictograms') {
+      if (token.attributes.item === 'pictograms') {
         return lyneIcons['accessibility-barrierefrei-left'];
       }
 
@@ -76,6 +99,11 @@ export default {
 </script>
 
 <style lang="scss">
+
+.default-size-token {
+  display: block;
+  background-color: black;
+}
 
 .icon-size-token {
   display: block;
