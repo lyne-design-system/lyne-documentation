16c4ceaa289cb09a24fe7d69ece7703ac6c7bc2e
Yves Tscherry <info@feerglas.com>
2021-08-14 18:07:44 +0200

    feat:  add page for border tokens

---
+++ src/pages/tokens/Border.vue
@@ -0,0 +1,100 @@
+<template>
+  <Layout>
+    <section class="section">
+      <div class="container">
+        <div class="content">
+          <h1 class="title is-1">Border</h1>
+
+          <div
+            v-for="(group, index) in $data.tokensGroups"
+            :key="index"
+          >
+            <h2>{{group.name}}</h2>
+
+            <TokensTable
+              :tokens="group.tokens"
+            >
+              <template v-slot="slotProps">
+                <span
+                  v-if="group.key === 'width'"
+                  class="border-token-width"
+                  :style="{borderWidth: slotProps.token.value}"
+                />
+                <span
+                  v-if="group.key === 'radius'"
+                  class="border-token-radius"
+                  :style="{borderRadius: slotProps.token.value}"
+                />
+              </template>
+            </TokensTable>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </Layout>
+</template>
+
+<script>
+import { designTokensByCategory } from '../../helpers/designToken';
+import TokensTable from '../../components/TokensTable.vue';
+
+const {
+  tokens
+} = require('lyne-design-tokens/dist/js/tokens-raw.json');
+
+const borderTokens = designTokensByCategory(tokens, 'border');
+
+borderTokens.map((_token) => {
+  const token = _token;
+
+  token.value = `${token.value}px`;
+
+  return token;
+});
+
+const widthTokens = borderTokens.filter((token) => token.attributes.type === 'width');
+const radiusTokens = borderTokens.filter((token) => token.attributes.type === 'radius');
+
+export default {
+  components: {
+    TokensTable
+  },
+  data() {
+    return {
+      tokensGroups: [
+        {
+          key: 'width',
+          name: 'border-width',
+          tokens: widthTokens
+        },
+        {
+          key: 'radius',
+          name: 'border-radius',
+          tokens: radiusTokens
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.border-token-width {
+  display: block;
+  width: 4rem;
+  height: 4rem;
+  border-color: black;
+  border-style: solid;
+}
+
+.border-token-radius {
+  display: block;
+  width: 4rem;
+  height: 4rem;
+  border: 1px solid black;
+}
+
+</style>
