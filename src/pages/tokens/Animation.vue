6a59b07bf8e8b49ee95682d24f82b35cc2761e97
Yves Tscherry <info@feerglas.com>
2021-08-14 18:07:52 +0200

    feat: add page for animation  tokens

---
+++ src/pages/tokens/Animation.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+    <section class="section">
+      <div class="container">
+        <div class="content">
+          <h1 class="title is-1">Animation</h1>
+
+          <TokensTable
+            :tokens="$data.tokens"
+            :hideExampleColumn="true"
+          />
+
+        </div>
+      </div>
+    </section>
+
+  </Layout>
+</template>
+
+<script>
+import { designTokensByCategory } from '../../helpers/designToken';
+import TokensTable from '../../components/TokensTable.vue';
+
+const {
+  tokens
+} = require('lyne-design-tokens/dist/js/tokens-raw.json');
+
+const animationTokens = designTokensByCategory(tokens, 'animation');
+
+export default {
+  components: {
+    TokensTable
+  },
+  data() {
+    return {
+      tokens: animationTokens
+    };
+  }
+};
+</script>
