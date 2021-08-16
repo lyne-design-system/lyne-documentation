72f72cffeb1040861dc248a78bec3bf6faa9631b
Yves Tscherry <info@feerglas.com>
2021-08-16 12:37:53 +0200

    feat: add variant for button

--- src/components.js
+++ src/components.js
@@ -4,9 +4,20 @@ const componentsData = [
     variants: [
       {
         attrs: {
+          label: 'Button',
+          size: 'large',
           variant: 'primary'
         },
+        slots: [],
         title: 'primary'
+      },
+      {
+        attrs: {
+          label: 'Button',
+          variant: 'primary'
+        },
+        slots: ['<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="m17.8436,12.1382-3.99-3.99196-.7072.70693,3.1366,3.13823H5v1h11.287l-3.1413,3.1555.7086.7056,3.99-4.008.3519-.3535-.3526-.3528z"></path></svg>'],
+        title: 'primary with icon'
       }
     ]
   },
