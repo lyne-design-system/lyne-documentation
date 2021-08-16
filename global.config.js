97b3cdcc8d99a7849245179dc18b58c90869648e
Yves Tscherry <info@feerglas.com>
2021-08-16 11:21:04 +0200

    chore: remove dato keys

--- global.config.js
+++ global.config.js
@@ -3,9 +3,6 @@ module.exports = {
   deploymentsJsonKeyProd: 'production',
   deploymentsJsonUrl: 'https://lyne-components-deployments.netlify.app/deployments.json',
   graphqlComponentsReadmeTypeName: 'MdDoc',
-  graphqlDatoFieldName: 'lyne',
-  graphqlDatoTypeName: 'lyneTypes',
-  graphqlDatoUrl: 'https://graphql.datocms.com',
   lyneComponentsDocumentationPath: 'node_modules/lyne-test/dist/documentation',
   lyneComponentsPrefix: 'lyne',
   lyneDocumentationContentTypeName: 'LyneContent'
