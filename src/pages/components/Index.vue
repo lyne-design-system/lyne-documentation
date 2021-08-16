1134d776984b7386f1cb4bd324a7bdbe4ef38b8b
Yves Tscherry <info@feerglas.com>
2021-08-16 11:23:40 +0200

    chore: remove auto rendered contents from components overview page

--- src/pages/components/Index.vue
+++ src/pages/components/Index.vue
@@ -4,39 +4,12 @@
       <div class="container">
         <h1 class="title is-1">Lyne Components</h1>
 
-        <div class="content">
-          <ul>
-
-            <li
-              v-for="(comp) in $page.lyne.allComponents"
-              :key="comp.id"
-            >
-
-              <g-link :to="`/components/${comp.componentName}/`">
-                {{comp.title}} ({{comp.componentName}})
-              </g-link>
-
-            </li>
-
-          </ul>
-        </div>
+        <p>List of components will follow...</p>
       </div>
     </section>
 
   </Layout>
 </template>
-
-<page-query>
-query {
-  lyne {
-    allComponents {
-      id
-      title
-      componentName
-    }
-  }
-}
-</page-query>
 
 <style>
 </style>
