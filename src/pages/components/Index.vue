22b45e77f7493f32ff81c1f2cd5fa1a36cb6638f
Yves Tscherry <info@feerglas.com>
2021-08-17 13:19:59 +0200

    feat: render links to components

--- src/pages/components/Index.vue
+++ src/pages/components/Index.vue
@@ -1,15 +1,33 @@
 <template>
   <Layout>
-    <section class="section">
-      <div class="container">
-        <h1 class="title is-1">Lyne Components</h1>
 
-        <p>List of components will follow...</p>
+    <section class="section">
+      <div class="content">
+        <h1 class="title is-1">Components</h1>
+
+        <ul>
+          <li
+            v-for="(comp) in $data.components"
+            :key="comp.name"
+          >
+            <g-link :to="'components/' + comp.name">{{comp.name}}</g-link>
+          </li>
+        </ul>
+
       </div>
     </section>
 
   </Layout>
 </template>
 
-<style>
-</style>
+<script>
+import components from '../../components';
+
+export default {
+  data() {
+    return {
+      components
+    };
+  }
+};
+</script>
