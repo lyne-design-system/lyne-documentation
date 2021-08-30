d1837a8d2129177b6e98b0fd7abcf7a28b2fcbf3
Yves Tscherry <info@feerglas.com>
2021-08-30 17:27:38 +0200

    fix: make link to about section instead of button

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,8 +12,9 @@
           <p>The Lyne Design System provides a common terminology, well defined processes, working code, design decisions, resources and human interface guidelines which all together define our shared, common language.</p>
            <!-- eslint-disable-next-line max-len -->
           <p>Lyne empowers people and unites teams to <span class="is-highlighted show-cash">work more efficiently</span> and <span class="is-highlighted show-cash">deliver more consistent experiences</span>.</p>
-          <p><g-link to="/about/" class="button is-primary is-highlighted show-cash">All about Lyne</g-link></p>
+
           <ul>
+            <li><g-link to="/about/">All about Lyne</g-link></li>
             <li><g-link to="/designing/">Start designing</g-link></li>
             <li><g-link to="/coding/">Start coding</g-link></li>
             <li><g-link to="/contributing/">Start contributing</g-link></li>
