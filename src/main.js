2b28f3862adad6a8f03204c7fd45adc6437848da
Yves Tscherry <info@feerglas.com>
2021-08-30 18:56:45 +0200

    chore: reorganize scss files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './styles/bulma.scss';
+import './styles/styles.scss';
 import Buefy from 'buefy';
 import { defineCustomElements } from 'lyne-test/loader';
 import DefLayout from './layouts/Default.vue';
