5860df441380b7bc470d4a97cc9af39b55f71dfb
Yves Tscherry <info@feerglas.com>
2021-09-01 11:30:32 +0200

    chore: use lyne title-component

--- src/pages/tokens/Animation.vue
+++ src/pages/tokens/Animation.vue
@@ -3,7 +3,7 @@
     <section class="section">
       <div class="container">
         <div class="content">
-          <h1 class="title is-1">Animation</h1>
+          <lyne-title level="1" text="Animation"></lyne-title>
 
           <TokensTable
             :tokens="$data.tokens"
