ecc6e026f5ee7af5175f2be8dd65882fe77d21fc
Marius Bleuer <marius.bleuer@unic.com>
2021-09-20 21:37:19 +0200

    fix: adding preload links and fixed asset location to avoid re-directs

--- src/main.js
+++ src/main.js
@@ -28,8 +28,39 @@ export default (Vue, {
   /* eslint-enable no-param-reassign */
 
   head.link.push({
-    href: 'https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css',
+    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@4.9.95/css/materialdesignicons.min.css',
     rel: 'stylesheet'
+  });
+
+  head.link.push({
+    as: 'font',
+    crossOrigin: '',
+    href: 'https://cdn.app.sbb.ch/fonts/v1_6_subset/SBBWeb-Roman.woff2',
+    rel: 'preload',
+    type: 'font/woff2'
+  });
+
+  head.link.push({
+    as: 'font',
+    crossOrigin: '',
+    href: 'https://cdn.app.sbb.ch/fonts/v1_6_subset/SBBWeb-Bold.woff2',
+    rel: 'preload',
+    type: 'font/woff2'
+  });
+
+  head.link.push({
+    as: 'style',
+    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@4.9.95/css/materialdesignicons.min.css',
+    rel: 'preload',
+    type: 'text/css'
+  });
+
+  head.link.push({
+    as: 'font',
+    crossOrigin: '',
+    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@4.9.95/fonts/materialdesignicons-webfont.woff2?v=4.9.95',
+    rel: 'preload',
+    type: 'font/woff2'
   });
 
   head.meta.push({
